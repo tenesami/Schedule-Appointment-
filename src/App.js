5a92417255a3d88e9ee26a0062008d5440825465
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-04-07 10:20:03 -0600

    Edit the app funciton and import Topic component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Topic from './Components/Topic'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Schedule App
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+return (
+<div className='container'>
+  <Topic/>
+</div>
+); 
 }
-
 export default App;
