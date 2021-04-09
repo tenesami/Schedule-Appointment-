a948ebd1e2abb1954a4e25fd01b113b73a2e3582
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-04-08 20:42:42 -0600

    add click event on the add button

--- src/Components/Topic.js
+++ src/Components/Topic.js
@@ -3,11 +3,14 @@ import Button from './Button'
 
 
 const Topic = ({ title}) => {
+  const onClick = () => {
+    console.log('click')
+  }
   return (
     <header className='header'>
       <h1>{title}</h1>
         {/* <button className='btn'>Add</button> */}
-        <Button color='blue' text='Add'/>
+        <Button color='blue' text='Add' onClick={onClick}/>
     </header>
   )
 }
