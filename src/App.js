20c0618c135872fa44af08ae3c53e5fa051188c1
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-04-08 20:45:38 -0600

    add task state on the app using hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import {useState} from 'react'
 import Topic from './Components/Topic'
+import Schedule from './Components/Schedule'
 
 const App = () => {
-
+const [tasks, setTasks] = useState(
+  [
+    {
+      id: 1,
+      text: 'Talk to Kids',
+      day: 'Feb 12th at 12:12am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Workout',
+      day: 'Nov 12th at 1:12pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Read Mini Habit',
+      day: 'Sep 21th at 3:12am',
+      reminder: true,
+    },
+  ]
+)
 return (
 <div className='container'>
   <Topic/>
+  <Schedule tasks={tasks}/>
 </div>
 ); 
 }
