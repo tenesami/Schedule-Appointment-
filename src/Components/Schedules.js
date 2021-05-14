f0be9855990dc982ea43605192c1b6031bd004fb
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-05-13 21:25:50 -0600

    passing onDelete as a prop inside in schedule

--- src/Components/Schedules.js
+++ src/Components/Schedules.js
@@ -1,11 +1,11 @@
 import Schedule from './Schedule'
 
-const Schedules = ({tasks}) => {
+const Schedules = ({tasks, onDelete}) => {
     return (
         <>
         {/* loop the tasks and pass the task properties to the shedule */}
         {tasks.map((task) => (
-            <Schedule key={task.id} task={task} />
+            <Schedule key={task.id} task={task} onDelete={onDelete} />
         )
         )}
         </>
