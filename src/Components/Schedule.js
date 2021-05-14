6132b28af3d9430be16563e3d95ed062733cf233
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-05-13 20:51:58 -0600

    instal icons and adding schedule compoment

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -1,9 +1,13 @@
-
+import {FaTimes} from 'react-icons/fa'
 
 const Schedule = ({task}) => {
     return (
         <div className='task'>
-            <h4> {task.text}</h4>
+            {/* dispaly the Schedule properties  */}
+            <h4> {task.text} <FaTimes style={{color: 'red',
+            cursor: 'pointer'}} />
+            </h4>
+            <p> {task.day}</p>
         </div>
     )
 }
--- src/Components/Schedules.js
+++ src/Components/Schedules.js
@@ -3,6 +3,7 @@ import Schedule from './Schedule'
 const Schedules = ({tasks}) => {
     return (
         <>
+        {/* loop the tasks and pass the task properties to the shedule */}
         {tasks.map((task) => (
             <Schedule key={task.id} task={task} />
         )
