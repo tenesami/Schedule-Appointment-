aa92d06b2eb4c61335bb92b48cb84f5c5c3b0e4e
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-05-14 16:30:41 -0600

    pass onToggle as props

--- src/Components/Schedules.js
+++ src/Components/Schedules.js
@@ -1,11 +1,12 @@
 import Schedule from './Schedule'
 
-const Schedules = ({tasks, onDelete}) => {
+const Schedules = ({tasks, onDelete, onToggle}) => {
     return (
         <>
         {/* loop the tasks and pass the task properties to the shedule */}
         {tasks.map((task) => (
-            <Schedule key={task.id} task={task} onDelete={onDelete} />
+            <Schedule key={task.id} task={task} 
+            onDelete={onDelete} onToggle={onToggle}/>
         )
         )}
         </>
