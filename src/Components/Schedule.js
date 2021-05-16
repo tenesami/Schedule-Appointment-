d25288c6d296b659d376d90346335845c9777d09
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2021-05-15 21:17:27 -0600

    pass onToggle props add conditons on add button

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -1,11 +1,14 @@
 import {FaTimes} from 'react-icons/fa'
 
-const Schedule = ({task, onDelete}) => {
+const Schedule = ({task, onDelete, onToggle}) => {
     return (
-        <div className='task'>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`}
+        onDoubleClick={() => onToggle(task.id)}>
             {/* dispaly the Schedule properties  */}
             <h4> {task.text} <FaTimes style={{color: 'red',
-            cursor: 'pointer'}} onClick={() => onDelete(task.id)} />
+            cursor : 'pointer'}} 
+            // onClick event to onDelete  
+            onClick={() => onDelete(task.id)} />
             </h4>
             <p> {task.day}</p>
         </div>
